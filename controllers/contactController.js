d26c15d2c468af32f0cce9d143f3258bb2b8128f
syfulsharif <sharif.syful@gmail.com>
2023-09-06 20:29:27 +0600

    Async Handler Middlewear added and functions got updated

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,58 +1,60 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
 
-const getContacts = (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: "Get all contacts",
   });
-};
+});
 
 //@desc Create new contacts
 //@route POST /api/contacts
 //@access public
 
-const createContact = (req, res) => {
-    console.log("The request body is ", req.body);
-    const {name, email, phone} = req.body;
-    if (!name || !email || !phone) {
-        res.status(400);
-        throw new Error("All fields are mandatory!");
-    }
+const createContact = asyncHandler(async (req, res) => {
+  console.log("The request body is ", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
   res.status(201).json({
     message: "Create contact",
   });
-};
+});
 
 //@desc Get an individual contact
 //@route GET /api/contacts/:id
 //@access public
 
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: `Get contact for id ${req.params.id}`,
   });
-};
+});
 
 //@desc Update an individual contact
 //@route PUT /api/contacts/:id
 //@access public
 
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: `Updated contact ${req.params.id}`,
   });
-};
+});
 
 //@desc Delete an individual contact
 //@route PUT /api/contacts/:id
 //@access public
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: `Deleted contact ${req.params.id}`,
   });
-};
+});
 
 module.exports = {
   getContacts,
