4753092defd9eb95b23865594c8168054a872a20
syfulsharif <sharif.syful@gmail.com>
2023-09-15 10:57:14 +0600

    Bcrypt Added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,24 @@
 const asyncHandler = require("express-async-handler");
-const Contact = require("../models/contactModel");
+const User = require("../models/userModel");
+const userModel = require("../models/userModel");
 
 //@desc Register a user
 //@route POST /api/users/register
 //@access public
 
 const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  const userAvailable = await userModel.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already registered");
+  }
+
+  
   res.json({
     message: "Register the user",
   });
